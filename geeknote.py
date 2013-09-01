34233e46910ea88d5752e03587cbc0c8684eb972
Adriano Gil <adrianogil.san@gmail.com>
2013-08-31 20:33:03 -0400

    Get note contents from a Notes object

--- geeknote.py
+++ geeknote.py
@@ -114,7 +114,7 @@ class GeekNote(object):
         userStoreHttpClient = THttpClient.THttpClient(self.userStoreUri)
         userStoreProtocol = TBinaryProtocol.TBinaryProtocol(userStoreHttpClient)
         GeekNote.userStore = UserStore.Client(userStoreProtocol)
-    
+
         self.checkVersion()
 
         return GeekNote.userStore
@@ -161,13 +161,13 @@ class GeekNote(object):
 
     def removeUser(self):
         return self.getStorage().removeUser()
-    
+
     """
     WORK WITH NOTEST
     """
     @EdamException
     def findNotes(self, keywords, count, createOrder=False):
-        
+
         noteFilter = NoteStore.NoteFilter(order=Types.NoteSortOrder.RELEVANCE)
         if createOrder:
             noteFilter.order = Types.NoteSortOrder.CREATED
@@ -508,7 +508,7 @@ class Notebooks(GeekNoteConnector):
 
 class Notes(GeekNoteConnector):
     """ Work with Notes """
-    
+
     findExactOnUpdate = False
     selectFirstOnUpdate = False
     def __init__(self, findExactOnUpdate=False, selectFirstOnUpdate=False):
@@ -535,7 +535,7 @@ class Notes(GeekNoteConnector):
         note = self._searchNote(note)
 
         inputData = self._parceInput(title, content, tags, notebook, note)
-        
+
         out.preloader.setMessage("Saving note...")
         result = self.getEvernote().updateNote(guid=note.guid, **inputData)
 
@@ -565,11 +565,21 @@ class Notes(GeekNoteConnector):
         self.connectToEvertone()
 
         note = self._searchNote(note)
-        
+
         out.preloader.setMessage("Loading note...")
         self.getEvernote().loadNoteContent(note)
 
         out.showNote(note)
+
+    def getNoteContent(self, note):
+        self.connectToEvertone()
+
+        note = self._searchNote(note)
+
+        out.preloader.setMessage("Loading note...")
+        self.getEvernote().loadNoteContent(note)
+
+        return note.content
 
     def _parceInput(self, title=None, content=None, tags=None, notebook=None, note=None):
         result = {
@@ -613,7 +623,7 @@ class Notes(GeekNoteConnector):
             if notepadGuid is None:
                 newNotepad = Notebooks().create(notebook)
                 notepadGuid = newNotepad.guid
-            
+
             result['notebook'] = notepadGuid
             logging.debug("Search notebook")
 
@@ -642,7 +652,7 @@ class Notes(GeekNoteConnector):
                 note = result.notes[0]
 
             else:
-                logging.debug("Choose notes: %s" % str(result.notes)) 
+                logging.debug("Choose notes: %s" % str(result.notes))
                 note = out.SelectSearchResult(result.notes)
 
         logging.debug("Selected note: %s" % str(note))
@@ -708,7 +718,7 @@ class Notes(GeekNoteConnector):
                 search = '"%s"' % search
 
             if content_search:
-                request += "%s" % search 
+                request += "%s" % search
             else:
                 request += "intitle:%s" % search
 
